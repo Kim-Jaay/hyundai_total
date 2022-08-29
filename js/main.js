bffef5c01899f44bd97a9b9917e587a84335f1af
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-08-29 16:46:04 +0900

    0829

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(function () {
-    ///////////////////////////////
+    /////////////////////////////////////
     $('.top_close_btn').on('click', function () {
-        $('.TopBanner').addClass('on')
+        $('.TopBanner').addClass('on');
+        $('.Section01').addClass('on');
     });
 
     $('.main_slider').slick({
@@ -10,15 +11,11 @@ $(function () {
 
     $(window).on('scroll', function () {
         var sct = $(window).scrollTop();
-        sct > 0 ? $('.Header').addClass('on')
-            : $('.Header').removeClass('on');
+        sct > 0
+            ? $('.Header').addClass('on')
+            : $('.Header').removeClass('on')
     })
 
 
-
-
-
-
-
-    ///////////////////////////////
+    /////////////////////////////////////
 })
